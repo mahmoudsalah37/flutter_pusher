00a6d7a940d38f3b02432c1dbe86aa0a2310b763
Ahmed Mohamed Nasser <50350016+ahmednasser74@users.noreply.github.com>
2021-03-17 14:50:05 +0200

    map screen added

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pusher/pages/login/login.dart';
+import 'package:flutter_pusher/pages/map/map_screen.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => MapScreen(),
+                ),
+              ),
+              child: Text('Map'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => LoginPage(),
+                ),
+              ),
+              child: Text('Chat'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
